0d1ed0a0e2e2dc0451240eb71f8236093fa09d8d
Kamal Sacranie <kamalsjunkmail@gmail.com>
2025-01-25 00:32:10 +0000

    Add yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,1 +1,17 @@
 require("git"):setup()
+require("bookmarks"):setup({
+    last_directory = { enable = true, persist = false },
+    persist = "vim",
+    desc_format = "full",
+    file_pick_mode = "hover",
+    notify = {
+        enable = true,
+        timeout = 1,
+        message = {
+            new = "New bookmark '<key>' -> '<folder>'",
+            delete = "Deleted bookmark in '<key>'",
+            delete_all = "Deleted all bookmarks",
+        },
+    },
+})
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })

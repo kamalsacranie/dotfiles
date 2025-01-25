f5c9261f14b87a41efdf459e2c05659afb3b7cf2
Kamal Sacranie <kamalsjunkmail@gmail.com>
2025-01-25 00:11:17 +0000

    fixup! fixup! Wallop commit

---
+++ sketchybar/plugins/clipboard.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+POPUP_CLICK_SCRIPT="sketchybar --set \$NAME popup.drawing=toggle"
+
+sketchybar --add item clipboard right                        \
+         --set clipboard script="python3 $PLUGIN_DIR/clipboard.py" \
+                    updates=on                             \
+                    update_freq=30                    \
+                    icon=                              \
+                    background.padding_right=15            \
+                    click_script="python3 $PLUGIN_DIR/clipboard.py && $POPUP_CLICK_SCRIPT" \
+                    icon.font="$FONT:Regular:18.0"       \
+         --add item clipboard.template left popup.clipboard \
+
